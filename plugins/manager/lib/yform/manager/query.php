97f269e1e9f94d2836e46515de3d6610bffd15e6
Alex Platter <alex.platter@gmail.com>
2020-10-29 09:49:47 +0100

    query->count() bugfix

--- plugins/manager/lib/yform/manager/query.php
+++ plugins/manager/lib/yform/manager/query.php
@@ -753,13 +753,8 @@ class rex_yform_manager_query implements IteratorAggregate, Countable
     public function count()
     {
         $query = clone $this;
-        $query
-            //->resetSelect() kreatif: removed
-            ->selectRaw('COUNT(*)', 'count')
-            ->resetOrderBy();
-
         $sql = rex_sql::factory();
-        $sql->setQuery($query->getQuery(), $query->getParams());
+        $sql->setQuery('SELECT COUNT(*) AS count FROM (' . $query->getQuery() . ') AS __SUBQUERY', $query->getParams());
 
         return $sql->getValue('count');
     }
